7164a2842b06739492a5f34e80348596c3cd12cf
syed afsar <47940477+syed-afsar@users.noreply.github.com>
2019-06-20 22:43:19 +0530

    STRUCTURAL DIRECTIVE *ngIf, *ngFor, *ngSwitch

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'STRUCTURE DIRCETIVES';
+
+  display = true;
+
+  // displays = false;
+
+  // displayss = true;
+
+  // colors = ["red", "blue", "green", "violet"];
+
+  // color = "violet" ;
+}
